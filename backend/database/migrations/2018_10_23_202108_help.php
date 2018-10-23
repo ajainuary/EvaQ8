cd5f47166368dc2d67db1afd9a56dde2598f7d99
Harsh <harsh.26020@gmail.com>
2018-10-24 02:07:24 +0530

    Initialized backend

---
+++ backend/database/migrations/2018_10_23_202108_help.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Help extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('help', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('phone_number', 14)->unique();
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('help');
+    }
+}
