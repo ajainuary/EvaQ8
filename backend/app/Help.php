3b51b219e552d97b200d7ed23aa199233e262d2e
Anurag Jain <ajainuary@gmail.com>
2018-10-24 14:13:27 +0530

    Added fields in models

--- backend/app/Help.php
+++ backend/app/Help.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Help extends Model
 {
-    //
+    public $timestamps = false;
+    protected $primaryKey = 'phone_number';
+    protected $table = 'helps';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+    	'phone_number', 'name', 'latitude', 'longtitude'
+    ];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'updated_time', 'created_time',
+    ];
 }
