cc5d48302c2c329157848bb583fff351c0640da3
Anurag Jain <ajainuary@gmail.com>
2018-10-26 00:10:05 +0530

    UI fix

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    EVAQ8
+                    EvaQ8
                 </div>
 
                 <div class="links">
