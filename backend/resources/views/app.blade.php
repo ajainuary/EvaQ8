14d0fa69da49e88f0ef0de85cbfe62ee358b397e
Harsh <harsh.26020@gmail.com>
2018-10-25 21:35:55 +0530

    Added Navbar

--- backend/resources/views/app.blade.php
+++ backend/resources/views/app.blade.php
@@ -11,8 +11,22 @@
 
     <!-- Custom styles for this template -->
     <link href="https://getbootstrap.com/docs/4.1/examples/checkout/form-validation.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   </head>
   <body class="bg-light">
+    <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="/">EVAQ8</a>
+    </div>
+    <div class="navbar-header">
+      <a class="navbar-brand" href="/help">Find Help</a>
+    </div>
+    <div class="navbar-header">
+      <a class="navbar-brand" href="/api/show_help">Show People</a>
+    </div>
+  </div>
+</nav>
     <div class="container">
         @yield('content')
     </div>
@@ -21,5 +35,7 @@
     <script src="https://getbootstrap.com/docs/4.1/assets/js/vendor/popper.min.js"></script>
     <script src="https://getbootstrap.com/docs/4.1/dist/js/bootstrap.min.js"></script>
     <script src="https://getbootstrap.com/docs/4.1/assets/js/vendor/holder.min.js"></script>
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </body>
 </html>
